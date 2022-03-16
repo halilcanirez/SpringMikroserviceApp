2cecf18945bddd5dc2439a34c7860395987e4568
Halil Can İrez <halil9819@gmail.com>
2022-03-16 22:44:29 +0300

    create advertisement service

--- advertisement-service/src/main/java/com/halil/secondhandapp/SecondHandAdvertisementApplication.java
+++ advertisement-service/src/main/java/com/halil/secondhandapp/SecondHandAdvertisementApplication.java
@@ -2,10 +2,12 @@ package com.halil.secondhandapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class SecondHandTicketApplication {
+@EnableEurekaClient
+public class SecondHandAdvertisementApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SecondHandTicketApplication.class,args);
+        SpringApplication.run(SecondHandAdvertisementApplication.class,args);
     }
 }
