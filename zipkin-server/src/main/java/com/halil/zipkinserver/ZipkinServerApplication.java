9decf6912b4fa068074169618be8467c8460c90d
Halil Can İrez <halil9819@gmail.com>
2022-03-22 16:45:50 +0300

    add zipkin server

--- zipkin-server/src/main/java/com/halil/zipkinserver/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/halil/zipkinserver/ZipkinServerApplication.java
@@ -2,7 +2,9 @@ package com.halil.zipkinserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
 
+@EnableZipkinServer
 @SpringBootApplication
 public class ZipkinServerApplication {
     public static void main(String[] args) {
